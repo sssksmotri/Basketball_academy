88fd0666a5cc268109e063119ed785262b487de3
kaoru <nsaidov530@gmail.com>
2025-02-28 03:13:26 +0300

    update swag

--- docs/docs.go
+++ docs/docs.go
@@ -2492,12 +2492,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
-	Host:             "localhost:8080",
-	BasePath:         "/",
+	Version:          "",
+	Host:             "",
+	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "Basketball Academy API",
-	Description:      "API для Basketball Academy",
+	Title:            "",
+	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
